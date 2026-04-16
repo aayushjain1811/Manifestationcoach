983373d760cd874a4b293a1e8fe8209eac7a0cf3
Ayush Jain <aayushjain1811@gmail.com>
2026-04-16 14:44:17 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("/*", cors());
+app.options(/.*/, cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
